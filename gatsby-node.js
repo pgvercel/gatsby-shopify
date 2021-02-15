9fc30e15a6ec8a9af33b20ebd923189a0f5ee4bc
Andrei Buhus <andrus_sheva@yahoo.com>
2021-02-15 19:00:16 +0000

    first commit

---
+++ gatsby-node.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+
+// allow us to use absolute paths: import @components
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  });
+};
+
+
+// create pages for each product after query data from Shopify
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const { data } = await graphql(`
+    {
+      allShopifyProduct {
+        edges {
+          node {
+            shopifyId
+            handle
+          }
+        }
+      }
+    }
+  `);
+
+  data.allShopifyProduct.edges.forEach(({ node }) => {
+    createPage({
+      path: `products/${node.handle}`,
+      context: {
+        shopifyId: node.shopifyId,
+      },
+      component: path.resolve('./src/templates/ProductTemplate/index.js'),
+    });
+  });
+};
